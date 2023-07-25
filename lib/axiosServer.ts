d1e87ce0c85ccfe8a6e0613aa3e743bed11b61b2
Monir-Ruet <monir81.mk@gmail.com>
2023-07-26 01:24:32 +0600

    first commit

---
+++ lib/axiosServer.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { cookies } from 'next/headers';
+const axiosServer = axios.create({
+    baseURL: process.env.backend,
+    withCredentials: true
+});
+
+axiosServer.interceptors.request.use(
+    (config) => {
+        const accessToken = cookies().get('auth_token')?.value;
+        if (accessToken) {
+            if (config.headers) config.headers.Authorization = "Bearer " + accessToken;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosServer;
